bda09a6749bdfd1125143a775219867b4c89ca05
noi <chatchawunj@gmail.com>
2018-10-04 11:50:30 +0000

    test

--- kbank/main.tf
+++ kbank/main.tf
@@ -8,50 +8,55 @@ terraform {
   required_version = "> 0.8.0"
 }
 
-provider "ibm" {}
-
-variable "public_ssh_key" {
-  description = "Public SSH key used to connect to the virtual guest"
-}
-
-variable "datacenter" {
-  description = "Softlayer datacenter where infrastructure resources will be deployed"
-}
-
-variable "hostname" {
-  description = "Hostname of the virtual instance (small flavor) to be deployed"
-  default     = "debian-small"
+provider "aws" {
+  version = "~> 1.8"
 }
 
 # This will create a new SSH key that will show up under the \
 # Devices>Manage>SSH Keys in the SoftLayer console.
-resource "ibm_compute_ssh_key" "orpheus_public_key" {
-  label      = "Orpheus Public Key"
-  public_key = "${var.public_ssh_key}"
-}
-
-variable "domain" {
-  description = "VM domain"
-}
-
 # Create a new virtual guest using image "Debian"
-resource "ibm_compute_vm_instance" "debian_small_virtual_guest" {
-  hostname                 = "${var.hostname}"
-  os_reference_code        = "DEBIAN_8_64"
-  domain                   = "${var.domain}"
-  datacenter               = "${var.datacenter}"
-  network_speed            = 10
-  hourly_billing           = true
-  private_network_only     = false
-  cores                    = 1
-  memory                   = 1024
-  disks                    = [25, 10, 20]
-  user_metadata            = "{\"value\":\"newvalue\"}"
-  dedicated_acct_host_only = false
-  local_disk               = false
-  ssh_key_ids              = ["${ibm_compute_ssh_key.orpheus_public_key.id}"]
+
+resource "aws_instance" "aws_instance" {
+  ami = "${var.aws_instance_ami}"
+  key_name = "${aws_key_pair.auth.id}"
+  instance_type = "${var.aws_instance_aws_instance_type}"
+  availability_zone = "${var.availability_zone}"
+  connection {
+    type = "ssh"
+    user = "${var.aws_instance_connection_user}"
+    password = "${var.aws_instance_connection_password}"
+    host = "${var.aws_instance_connection_host}"
+  }
+  subnet_id  = "${aws_subnet.subnet.id}"
+  tags {
+    Name = "${var.aws_instance_name}"
+  }
 }
 
-output "vm_ip" {
-  value = "Public : ${ibm_compute_vm_instance.debian_small_virtual_guest.ipv4_address}"
+resource "tls_private_key" "ssh" {
+    algorithm = "RSA"
 }
+
+resource "aws_key_pair" "auth" {
+    key_name = "aws-temp-public-key"
+    public_key = "${tls_private_key.ssh.public_key_openssh}"
+}
+
+resource "aws_vpc" "default" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "${var.network_name_prefix}"
+  }
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id = "${aws_vpc.default.id}"
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "${var.availability_zone}"
+  tags {
+    Name = "Main"
+  }
+}
+
--- kbank/variables.tf
+++ kbank/variables.tf
@@ -4,3 +4,41 @@
 ##
 #####################################################################
 
+variable "aws_instance_ami" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_aws_instance_type" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "availability_zone" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_name" {
+  type = "string"
+  description = "Generated"
+}
+
+variable "aws_instance_connection_user" {
+  type = "string"
+  default = "root"
+}
+
+variable "aws_instance_connection_password" {
+  type = "string"
+}
+
+variable "aws_instance_connection_host" {
+  type = "string"
+}
+
+variable "network_name_prefix" {
+  type = "string"
+  description = "Generated"
+}
+
